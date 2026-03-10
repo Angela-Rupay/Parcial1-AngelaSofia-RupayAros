572797c5ac5528cc46d2a56503628bdd5c0ead7b
Angela-Rupay <u20242227383@usco.edu.co>
2026-03-09 19:09:17 -0500

    carga archivo js

--- J_Parcial.js
+++ J_Parcial.js
@@ -1,13 +1,22 @@
 function cambiarTexto() {
 
-    document.getElementByID("descripcion").innerHTML = "Ahora estás viendo nuestras ofertas!"
+    document.getElementById("descripcion").innerHTML = "Ahora estás viendo nuestras ofertas!";
 
 }
 
 function modoOscuro() {
 
-    document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"
+    if (document.body.style.backgroundColor == "black") {
+
+        document.body.style.backgroundColor = "white"
+        document.body.style.color = "black"
+
+    } else {
+
+        document.body.style.backgroundColor = "black"
+        document.body.style.color = "white"
+
+    }
 
 }
 
@@ -20,15 +29,17 @@ function agregarCarrito() {
 
 }
 
-function enviarFormulario() {
+function enviarFormulario(){
 
-    let nombre = document.getElementById("nombre").value
-    let correo = document.getElementById("correo").value
+let nombre = document.getElementById("nombre").value
+let apellidos = document.getElementById("apellidos").value
+let edad = document.getElementById("edad").value
+let genero = document.getElementById("genero").value
+let direccion = document.getElementById("direccion").value
+let telefono = document.getElementById("telefono").value
+let correo = document.getElementById("correo").value
 
-    if (nombre == "") {
-        alert("El nombre es obligatorio")
-    }
-
-    console.log("Formulario enviado")
+let regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+let regexTelefono = /^[0-9]{7,10}$/
 
 }
